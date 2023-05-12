d57a86052eae0c21b897abc0f7a02ec8e8ed9b3b
BurakKurt <krtburak@outlook.com>
2023-05-12 19:43:43 +0300

    feature(preloader): created and project complated

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const playStopBtn = document.querySelector(".play-stop-btn");
 const playStopBtnSlider = document.querySelector(".play-stop-btn .btn-slider");
 const headerBackVideo = document.querySelector(".video-container");
+const preloader = document.querySelector(".preloader-gif");
 
 playStopBtn.addEventListener("click", function(){
   playStopBtnSlider.classList.toggle("active");
   if(!playStopBtnSlider.classList.contains("active")){
     headerBackVideo.play();
   } else headerBackVideo.pause();
+})
+
+window.addEventListener("load", function(){
+  preloader.classList.add("hidden");
 })
